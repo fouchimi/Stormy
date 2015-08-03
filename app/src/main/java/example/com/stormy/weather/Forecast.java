6801e841df798e187e8b2a5c0e80eb2784d9adb1
Ousmane <ousmanefouchimi1990@gmail.com>
2015-08-03 12:42:13 -0400

    Initial commit

---
+++ app/src/main/java/example/com/stormy/weather/Forecast.java
@@ -0,0 +1,37 @@
+package example.com.stormy.weather;
+
+/**
+ * Created by LeBoss on 7/31/2015.
+ */
+public class Forecast {
+
+    public Forecast() {}
+
+    private Current mCurrent;
+    private Hour[] mHourlyForecast;
+    private Day[] mDailyForecast;
+
+    public Current getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(Current current) {
+        mCurrent = current;
+    }
+
+    public Hour[] getHourlyForecast() {
+        return mHourlyForecast;
+    }
+
+    public void setHourlyForecast(Hour[] hourlyForecast) {
+        mHourlyForecast = hourlyForecast;
+    }
+
+    public Day[] getDailyForecast() {
+        return mDailyForecast;
+    }
+
+    public void setDailyForecast(Day[] dailyForecast) {
+        mDailyForecast = dailyForecast;
+    }
+}
---
+++ app/src/main/java/example/com/stormy/weather/Hour.java
@@ -0,0 +1,55 @@
+package example.com.stormy.weather;
+
+/**
+ * Created by LeBoss on 7/31/2015.
+ */
+public class Hour {
+
+    private long mTime;
+    private String mSummary;
+    private double mTemperature;
+    private String mIcon;
+    private String mTimeZone;
+
+    public Hour() {}
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+}
